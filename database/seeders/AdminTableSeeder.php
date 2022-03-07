b870437b1273e55e6da4d741059ed1ceeef5546f
Ahmed Gad <ahmed.ali-gad@outlook.com>
2022-03-07 03:25:17 +0200

    Add New Data To Admins Seeder

--- database/seeders/AdminTableSeeder.php
+++ database/seeders/AdminTableSeeder.php
@@ -14,10 +14,23 @@ class AdminTableSeeder extends Seeder
      */
     public function run()
     {
+        // Super Admin
         Admin::create(['name' => 'Super Admin', 'email' => 'admin@dashboard.com',
             'role' => 'super_admin', 'password' => bcrypt('dashboard')]);
-
+        // Admins
         Admin::create(['name' => 'Engineering Admin', 'email' => 'admin@engineering.com',
             'role' => 'admin', 'password' => bcrypt('engineering'), 'collage_id' => 1]);
+        Admin::create(['name' => 'Pharmacy Admin', 'email' => 'admin@pharmacy.com',
+            'role' => 'admin', 'password' => bcrypt('pharmacy'), 'collage_id' => 11]);
+        Admin::create(['name' => 'Nursing Admin', 'email' => 'admin@nursing.com',
+            'role' => 'admin', 'password' => bcrypt('nursing'), 'collage_id' => 12]);
+
+        // Staff
+        Admin::create(['name' => 'Engineering Member', 'email' => 'engineering@staff.com',
+            'role' => 'staff', 'password' => bcrypt('engineering'), 'collage_id' => 1]);
+        Admin::create(['name' => 'Pharmacy Member', 'email' => 'pharmacy@staff.com',
+            'role' => 'staff', 'password' => bcrypt('pharmacy'), 'collage_id' => 11]);
+        Admin::create(['name' => 'Nursing Member', 'email' => 'nursing@staff.com',
+            'role' => 'staff', 'password' => bcrypt('nursing'), 'collage_id' => 12]);
     }
 }
