ad365151248a107f9a2148f052f6153a039cc50a
Ahmed Gad <ahmed.ali-gad@outlook.com>
2022-03-07 03:26:56 +0200

    Add EBooks Controller

---
+++ app/Http/Controllers/Admin/EBooksController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\EBook;
+use Illuminate\Http\Request;
+
+class EBooksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $books = EBook::whereStaffId(auth()->id())->paginate(10);
+
+        return view('admin.e_books.index', compact('books'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\EBook  $eBook
+     * @return \Illuminate\Http\Response
+     */
+    public function show(EBook $eBook)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\EBook  $eBook
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(EBook $eBook)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\EBook  $eBook
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, EBook $eBook)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\EBook  $eBook
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(EBook $eBook)
+    {
+        //
+    }
+}
