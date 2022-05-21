8c8e45cd4329fdc9f72b51c85ed731f15c38077f
alaa ashraf <alaa_ashraf21799@yahoo.com>
2022-05-22 01:01:42 +0200

    linkEdit

--- resources/views/front/complain.blade.php
+++ resources/views/front/complain.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+@section('page.title', 'الشكاوي ')
 @section('content')
 
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,13 +9,9 @@
 
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,400i,500,500i,600,700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
-      crossorigin="anonymous">
 
 	<link rel="stylesheet" href="/front/css/studiare-assets.min.css">
 	<link rel="stylesheet" type="text/css" href="/fonts/font-awesome/font-awesome.min.css" media="screen">
-	<link rel="stylesheet" type="text/css" href="/fonts/elegant-icons/style.css" media="screen">
 	<link rel="stylesheet" type="text/css" href="/fonts/iconfont/material-icons.css" media="screen">
 	<link rel="stylesheet" type="text/css" href="/front/css/style.css">
 	<link rel="stylesheet" type="text/css" href="/front/css/rtl.css">
