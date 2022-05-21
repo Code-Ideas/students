6cca19076d94104592101d5fb9a46cfbd454c279
Ahmed Gad <ahmed.ali-gad@outlook.com>
2022-05-22 00:12:56 +0200

    Edit Typo

--- resources/views/front/clinic.blade.php
+++ resources/views/front/clinic.blade.php
@@ -8,7 +8,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link href="https://fonts.googleapis.com/css?family=Roboto:400,700,900&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="/fonts/icomoon/style.css">
 <link href="{{ asset('/admin/css/app.css') }}" rel="stylesheet" type="text/css">
 </head>
 
