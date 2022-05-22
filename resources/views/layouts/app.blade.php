e1feb00bafaf50bee563caa6fb38c509ae71e631
alaa ashraf <alaa_ashraf21799@yahoo.com>
2022-05-22 02:02:51 +0200

    link2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,9 +9,11 @@
 
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
+      crossorigin="anonymous">
 
 	<link rel="stylesheet" href="/front/css/studiare-assets.min.css">
-	<link rel="stylesheet" type="text/css" href="/fonts/font-awesome/font-awesome.min.css" media="screen">
+	<!-- <link rel="stylesheet" type="text/css" href="/fonts/font-awesome/font-awesome.min.css" media="screen"> -->
 	<link rel="stylesheet" type="text/css" href="/fonts/iconfont/material-icons.css" media="screen">
 	<link rel="stylesheet" type="text/css" href="/front/css/style.css">
 	<link rel="stylesheet" type="text/css" href="/front/css/rtl.css">
