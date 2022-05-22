d3c3f3d472fdc239865606c0abc167e28e69a086
Ahmed Gad <ahmed.ali-gad@outlook.com>
2022-05-22 10:42:26 +0200

    Change 404 Content

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,7 +1,23 @@
-@extends('layouts.app')
-@section('content')
-<section class="py-32 error-404-page">
-    <img class="object-contain w-full h-96" src="/admin/img/illustrations/404.svg"> 
-    <h1 class="mt-10 text-3xl font-semibold text-center text-green-500">عذراً لا يمكن العثور على هذه الصفحة</h1>
-</section>
-@endsection
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+    <title>خدمات الطلاب</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link href="/front/css/loginForm.css" rel="stylesheet" >
+</head>
+<body>
+<!-- Container -->
+<div class="wrapper fadeInDown">
+    <div id="formContent">
+        <section class="py-32 error-404-page">
+            <img class="object-contain w-full h-96" src="/admin/img/illustrations/404.svg">
+            <h1 class="mt-10 text-3xl font-semibold text-center text-green-500">عذراً لا يمكن العثور على هذه الصفحة</h1>
+            <h2><a href="{{ route('home') }}" class="btn btn-primary">العوده للرئيسية</a></h2>
+        </section>
+    </div>
+</div>
+</body>
+</html>
